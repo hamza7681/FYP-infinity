30d081f039c7de26f467372aee110606653cafda
asma-ismail10 <asmaismail202020@gmail.com>
2023-05-07 18:45:26 +0500

    update

--- client/src/Components/Home/Slider.js
+++ client/src/Components/Home/Slider.js
@@ -3,48 +3,7 @@ import { motion } from "framer-motion";
 import pic1 from "../../Assets/e learning.jpg";
 import pic2 from "../../Assets/game.jpeg";
 import pic3 from "../../Assets/python-1.jpeg";
-import pic4 from "../../Assets/iot.jpeg";
 
-// const images = [
-//   { src: pic1, text: "First image" },
-//   { src: pic2, text: "Second image" },
-//   { src: pic3, text: "Third image" },
-//   { src: pic4, text: "Fourth image" },
-// ];
-// const Slider = () => {
-//   const [currentSlide, setCurrentSlide] = useState(0);
-
-//   const nextSlide = () => {
-//     setCurrentSlide(currentSlide === images.length - 1 ? 0 : currentSlide + 1);
-//   };
-
-//   useEffect(() => {
-//     const timer = setTimeout(() => {
-//       nextSlide();
-//     }, 2000);
-
-//     return () => clearTimeout(timer);
-//   }, [currentSlide]);
-
-//   return (
-//     <div className="relative w-full h-[200px] md:h-[660px]">
-//       {images.map((image, index) => (
-//         <div
-//           key={index}
-//           className={`absolute top-0 left-0 w-full h-[80%] transition-opacity duration-1000 ${
-//             index === currentSlide ? "opacity-100" : "opacity-0"
-//           }`}
-//         >
-//           <img
-//             src={image.src}
-//             alt={image.text}
-//             className="object-fit w-full md:h-[600px]"
-//           />
-//         </div>
-//       ))}
-//     </div>
-//   );
-// };
 
 const carouselItems = [
   {
@@ -77,7 +36,6 @@ const Slider = () => {
       backgroundImage: `url(${carouselItems[activeIndex].image})`,
       backgroundRepeat: "no-repeat",
       backgroundSize: "cover",
-      opacity: "0.25",
     };
   
     return (
@@ -92,7 +50,7 @@ const Slider = () => {
           </motion.div>
         </div>
         <div className="flex-row m-[1%] opacity-100">
-          <img src={carouselItems[activeIndex].image} alt=""  className="h-96 w-[100%] opacity-100!important"/>
+          <img src={carouselItems[activeIndex].image} alt=""  className="h-96 w-[100%]"/>
         </div>
       </div>
     );
